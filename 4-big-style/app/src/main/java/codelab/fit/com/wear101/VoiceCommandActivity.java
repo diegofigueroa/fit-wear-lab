6a7754a20b144209f0cd4bd0d1d18bcd4f62c57f
Diego Figueroa <diegofigueroa99@gmail.com>
2014-10-08 19:58:29 -0600

    add project for step 4

---
+++ 4-big-style/app/src/main/java/codelab/fit/com/wear101/VoiceCommandActivity.java
@@ -0,0 +1,63 @@
+package codelab.fit.com.wear101;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.RemoteInput;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class VoiceCommandActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_command);
+
+        final CharSequence dictatedString = getMessageText();
+        final TextView textView = (TextView) findViewById(R.id.text_vew);
+
+        textView.setText(dictatedString);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.voice_command, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Obtain the intent that started this activity by calling
+     * Activity.getIntent() and pass it into this method to
+     * get the associated voice input string.
+     */
+
+    private CharSequence getMessageText() {
+        final Intent intent = getIntent();
+        final Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+
+        if (remoteInput != null) {
+            return remoteInput.getCharSequence(EXTRA_VOICE_REPLY);
+        }
+
+        return "Uhh? Say what?";
+    }
+
+    private final String EXTRA_VOICE_REPLY = "extra_voice_reply";
+}
