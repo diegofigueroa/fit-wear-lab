f57d4efbe160a989cf5b4a6f6e742924dc1e553f
Diego Figueroa <diegofigueroa99@gmail.com>
2014-10-08 20:18:40 -0600

    add project for step 5

---
+++ 5-notification-groups/app/src/main/java/codelab/fit/com/wear101/MainActivity.java
@@ -0,0 +1,260 @@
+package codelab.fit.com.wear101;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.RemoteInput;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showSimpleNotification(final View button) {
+        createSimpleNotification("Test", "Hi I'm a simple notification, you should be able to se mee on your watch!");
+    }
+
+    public void showSimpleNotificationWithAction(final View button) {
+        createNotificationWithAction("Test", "Hi I'm a simple notification, with some actions.");
+    }
+
+    public void showSimpleNotificationWithWearAction(final View button) {
+        createNotificationWithWearAction("Test", "Hi I'm a simple notification, with actions too!");
+    }
+
+    public void showSimpleNotificationWithVoiceAction(final View button) {
+        createNotificationWithVoiceAction("Test", "Hi! Are you there? Want to say something?");
+    }
+
+    public void showBigStyleNotification(final View button) {
+        createBigStyleNotification("Test", "This is a big style notification, you should see this by default. How cool is that, right? Adding more text, because why not?");
+    }
+
+    public void showNotificationGroup(final View button) {
+        createNotificationGroup("Test Group", "I'm a nice and cool notification!", "Hey I'm a cool notification too!");
+    }
+
+
+
+    private void createSimpleNotification(final String title, final String content) {
+        // An id for the notification to be showed, it should be unique.
+        final int notificationId = 001;
+
+        // Build intent for notification content
+        final Intent viewIntent = new Intent(this, MainActivity.class);
+        final PendingIntent viewPendingIntent = PendingIntent.getActivity(this, 0, viewIntent, 0);
+
+        final NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setContentIntent(viewPendingIntent);
+
+        // Get an instance of the NotificationManager service
+        final NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // Builds the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
+    }
+
+    private void createNotificationWithAction(final String title, final String content) {
+        // An id for the notification to be showed, it should be unique.
+        final int notificationId = 002;
+
+        // Build an intent for an action to view a map
+        final Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+        final Uri geoUri = Uri.parse("geo:0,0?q=");
+        mapIntent.setData(geoUri);
+
+        final PendingIntent mapPendingIntent = PendingIntent.getActivity(this, 0, mapIntent, 0);
+        final PendingIntent viewPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+
+        final NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setContentIntent(viewPendingIntent)
+                .setAutoCancel(true)
+                .addAction(R.drawable.ic_launcher, "View map", mapPendingIntent);
+
+        // Get an instance of the NotificationManager service
+        final NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // Builds the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
+    }
+
+    private void createNotificationWithWearAction(final String title, final String content) {
+        // An id for the notification to be showed, it should be unique.
+        final int notificationId = 003;
+
+        // Build an intent for an action to view a map
+        final Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+        final Uri geoUri = Uri.parse("geo:0,0?q=");
+        mapIntent.setData(geoUri);
+
+        final PendingIntent mapPendingIntent = PendingIntent.getActivity(this, 0, mapIntent, 0);
+        final PendingIntent viewPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+
+        // Create a wear only action to extend the notification
+        final NotificationCompat.Action action = new NotificationCompat.Action.Builder(R.drawable.ic_launcher, "View map", mapPendingIntent).build();
+
+        final NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setContentIntent(viewPendingIntent)
+                .setAutoCancel(true)
+                .addAction(R.drawable.ic_launcher, "Open map", mapPendingIntent)
+                .extend(new NotificationCompat.WearableExtender().addAction(action));
+
+        // Get an instance of the NotificationManager service
+        final NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // Builds the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
+    }
+
+    private void createNotificationWithVoiceAction(final String title, final String content) {
+        // An id for the notification to be showed, it should be unique.
+        final int notificationId = 004;
+
+        // Build a remote input to enable voice capture
+        final RemoteInput remoteInput = new RemoteInput.Builder(EXTRA_VOICE_REPLY)
+                .setLabel("Say something!")
+                .build();
+
+        // Create an intent for the reply action
+        final Intent replyIntent = new Intent(this, VoiceCommandActivity.class);
+        final PendingIntent replyPendingIntent = PendingIntent.getActivity(this, 0, replyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Create the reply action and add the remote input
+        final NotificationCompat.Action action = new NotificationCompat.Action.Builder(R.drawable.ic_launcher, "Say something!", replyPendingIntent)
+                .addRemoteInput(remoteInput)
+                .build();
+
+        // Build the notification and add the action via WearableExtender
+        final NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setAutoCancel(true)
+                .extend(new NotificationCompat.WearableExtender().addAction(action));
+
+        // Get an instance of the NotificationManager service
+        final NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // Builds the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
+    }
+
+    private void createBigStyleNotification(final String title, final String content) {
+        // An id for the notification to be showed, it should be unique.
+        final int notificationId = 005;
+
+        // Build an intent for an action to view a map
+        final Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+        final Uri geoUri = Uri.parse("geo:0,0?q=");
+        mapIntent.setData(geoUri);
+
+        // Specify the 'big view' content to display the long
+        // description that may not fit the normal content text.
+        final NotificationCompat.BigTextStyle bigStyle = new NotificationCompat.BigTextStyle();
+        bigStyle.setBigContentTitle("A big notification");
+        bigStyle.bigText(content);
+
+        final PendingIntent mapPendingIntent = PendingIntent.getActivity(this, 0, mapIntent, 0);
+        final PendingIntent viewPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+
+        final NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(title)
+                .setContentIntent(viewPendingIntent)
+                .setAutoCancel(true)
+                .addAction(R.drawable.ic_launcher, "View Map", mapPendingIntent)
+                .setStyle(bigStyle);
+
+        // Get an instance of the NotificationManager service
+        final NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // Build the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
+    }
+
+    private void createNotificationGroup(final String title, final String content1, final String content2) {
+        // An id for the notification to be showed, it should be unique.
+        final int notificationId = 006;
+        final String GROUP_KEY_EMAILS = "group_key_emails";
+
+        // Build a notification, setting the group appropriately
+        final Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle(title)
+                .setContentText(content1)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setGroup(GROUP_KEY_EMAILS)
+                .build();
+
+        // Build a second notification, setting the group appropriately
+        final Notification notification2 = new NotificationCompat.Builder(this)
+                .setContentTitle("Another one!")
+                .setContentText(content2)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setGroup(GROUP_KEY_EMAILS)
+                .build();
+
+
+        // Create an InboxStyle notification
+        final Notification summary = new NotificationCompat.Builder(this)
+                .setContentTitle("2 new notifications")
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setStyle(new NotificationCompat.InboxStyle()
+                        .addLine("Homer Simpson   Do'h!")
+                        .addLine("Milhouse   Re: Skinner prank")
+                        .setBigContentTitle("2 new messages")
+                        .setSummaryText("bart.simpson@gmail.com"))
+                .setGroup(GROUP_KEY_EMAILS)
+                .setGroupSummary(true)
+                .build();
+
+        // Issue the notification
+        final NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(notificationId, notification);
+        notificationManager.notify(notificationId + 1, notification2);
+        notificationManager.notify(notificationId + 2, summary);
+    }
+
+
+    private final String EXTRA_VOICE_REPLY = "extra_voice_reply";
+}
